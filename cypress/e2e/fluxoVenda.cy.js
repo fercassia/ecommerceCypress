38d9bcfd02a906ecc74ee136a8138a96c8caecdc
Mateus Neres <mponeres3@gmail.com>
2024-10-24 20:46:14 -0300

    adicionado template fluxo de venda

---
+++ cypress/e2e/fluxoVenda.cy.js
@@ -0,0 +1,18 @@
+import {
+    homePage,
+  } from '../support/pages/index';
+  
+  
+  beforeEach(() => {
+    cy.visit("");
+  });
+  
+  describe('Fluxo de venda', () => {
+    context('principal', () => {
+      it('Fluxo de venda com sucesso! ', () => {
+        //Arrange
+        //Action
+        //Assert
+      });
+    });
+  });
